5ae688ff61d43397569fb9dbf6d4e25323f6fe51
Febin John James <jamesfebin@gmail.com>
2015-07-28 19:58:18 +0530

    Gmail Scraping updates

--- gscrapweb/admin.py
+++ gscrapweb/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from models import Tracks
 # Register your models here.
+
+
+class TracksAdmin(admin.ModelAdmin):
+      list_display    = ['type', 'author', 'author_link', 'link','title']
+
+admin.site.register(Tracks, TracksAdmin)
