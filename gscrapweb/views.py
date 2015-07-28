af5d84b389d62a948876f275c28629d3a45d7e34
Febin John James <jamesfebin@gmail.com>
2015-07-28 11:57:11 +0530

    Navbar changes

--- gscrapweb/views.py
+++ gscrapweb/views.py
@@ -27,8 +27,8 @@ def home(request):
 def sync(request):
 	if request.user and request.user.is_anonymous() is False and request.user.is_superuser is False:
 		user = UserSocialAuth.objects.get(user=request.user,provider="google-oauth2")
-		print instance
-		print instance['extra_data']
+		print user
+		print user['extra_data']
 		return render_to_response('sync.html')
 		'''
 		google = request.user.social_auth.get(provider='google-oauth2')
