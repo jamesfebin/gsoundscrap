47fb40f00be703bed1b6b25ca345f780683018c6
Febin John James <jamesfebin@gmail.com>
2015-07-28 11:14:36 +0530

    Navbar changes

--- gscrapweb/views.py
+++ gscrapweb/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from social.apps.django_app.default.models import UserSocialAuth
 
 # Create your views here.
 
 def home(request):
 
 	soundcloud = False
-	if request.user and request.user.is_anonymous() is False and request.user.is_superuser is False:		
+	if request.user and request.user.is_anonymous() is False and request.user.is_superuser is False:
+		print request.user.get_username()	
+		print request.user	
 		try:
 			soundcloud = request.user.social_auth.get(provider='soundcloud')
 			if soundcloud:
@@ -26,4 +29,11 @@ def home(request):
 def sync(request):
 	if request.user and request.user.is_anonymous() is False and request.user.is_superuser is False:
 		google = request.user.social_auth.get(provider='google-oauth2')
+		if google: 
+			access_token = google['access_token']
+			response = requests.get(
+			    'https://www.googleapis.com/gmail/v1/users/jamesfebin%40gmail.com/messages',
+			    params={'access_token': 'ya29.vgFEY0uezVNr7Zmtusrwr51VZzVLq83xH6D-oEpjC2uI3NnUddDp3XIQbvmWrk2tJX_bjw','q':' boutline after:2010/01/01 before:2015/06/30'}
+			)
+
 
