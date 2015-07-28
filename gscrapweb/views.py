953c5a99d4dc8f88af6a96e6b67787f20560b81f
Febin John James <jamesfebin@gmail.com>
2015-07-28 11:47:07 +0530

    Navbar changes

--- gscrapweb/views.py
+++ gscrapweb/views.py
@@ -9,10 +9,6 @@ def home(request):
 
 	soundcloud = False
 	if request.user and request.user.is_anonymous() is False and request.user.is_superuser is False:
-		print request.user.get_username()	
-		print 'sup'
-		print request.user	
-		print 'yup'
 		try:
 			soundcloud = request.user.social_auth.get(provider='soundcloud')
 			if soundcloud:
@@ -30,8 +26,9 @@ def home(request):
 
 def sync(request):
 	if request.user and request.user.is_anonymous() is False and request.user.is_superuser is False:
-		instance = UserSocialAuth.objects.get(user=request.user)
+		user = UserSocialAuth.objects.get(user=request.user.get_user(),provider="google-oauth2")
 		print instance
+		print instance['extra_data']
 		return render_to_response('sync.html')
 		'''
 		google = request.user.social_auth.get(provider='google-oauth2')
