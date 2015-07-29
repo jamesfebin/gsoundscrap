76a762765008a7b8e8aace4e218e4a03870590d7
Febin John James <jamesfebin@gmail.com>
2015-07-29 19:13:04 +0530

    Worker Added

--- gscrapweb/tasks.py
+++ gscrapweb/tasks.py
@@ -42,7 +42,7 @@ def fetch_youtube_video_info(url,user,domain):
 					track = Track.objects.get(link=url,user_id=user.id)
 			except Track.DoesNotExist:
 					try:
-						Track.objects.create(title=title,thumbnail=thumbnail,author_link=author_url,author=author_name,track_type='youtube',url=link,user_id=user,embed=html)
+						Track.objects.create(title=title,thumbnail=thumbnail,author_link=author_url,author=author_name,track_type='youtube',url=url,user_id=user,embed=html)
 						print 'added'
 					except Exception, e:
 						print e
