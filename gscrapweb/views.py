8f7332ea1366a9e7ccf2d0aede2d6a421ffbd893
Febin John James <jamesfebin@gmail.com>
2015-07-31 16:26:30 +0530

    SoundCloud Added

--- gscrapweb/views.py
+++ gscrapweb/views.py
@@ -16,9 +16,8 @@ def home(request):
 	soundcloud = False
 	if request.user and request.user.is_anonymous() is False and request.user.is_superuser is False:
 		try:
-			soundcloud = request.user.social_auth.get(provider='soundcloud')
+			soundcloud = UserSocialAuth.objects.get(user=request.user,provider="soundcloud")
 			if soundcloud:
-				backend = soundcloud.get_backend_instance()
 				soundcloud = True
 		except Exception, e:
 			#Nothing to worry , Sound cloud isn't connected
